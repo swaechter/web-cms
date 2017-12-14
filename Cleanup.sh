8b08950b133bdb7dec0959b0c747ba3a645bea05
Simon Wächter <simon.waechter@secacon.com>
2017-12-14 09:45:48 +0100

    Improve the cleanup script

--- Cleanup.sh
+++ Cleanup.sh
@@ -8,10 +8,13 @@ sudo rm -rf .git/
 sudo rm -rf sql/
 sudo rm -rf tests/
 sudo rm -rf coverage/
-sudo rm composer.phar
-sudo rm index_tests.php
-sudo rm index_documentation.php
-sudo rm phpunit.xml
+sudo rm -f .gitignore
+sudo rm -f README.md
+sudo rm -f composer.json
+sudo rm -f composer.phar
+sudo rm -f index_tests.php
+sudo rm -f index_documentation.php
+sudo rm -f phpunit.xml
 
 # Delete the script itself
 sudo rm Cleanup.sh
